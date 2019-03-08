7143929dc3748308644830407f6fc4ae35625f05
nathri <nathrijacobs@gmail.com>
2019-03-08 12:52:56 +0200

    Got tests working

--- src/bicycles/Bicycle.java
+++ src/bicycles/Bicycle.java
@@ -4,15 +4,15 @@ public class Bicycle {
     private int speed = 0;
 
     public void accelerate(){
-        speed += 5;
+        this.speed += 5;
     }
     public void brake(){
-        speed -= 3;
+        this.speed -= 3;
     }
-    public int currentSpeed(){
-        return speed;
+    public int getSpeed(){
+        return this.speed;
     }
     public void stop(){
-        speed = 0;
+        this.speed = 0;
     }
 }

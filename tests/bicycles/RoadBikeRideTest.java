d34ce889e43ae265079dd48b0afe7b1a6775c22e
nathri <nathrijacobs@gmail.com>
2019-03-11 16:58:30 +0200

    Created another test to roadbike

--- tests/bicycles/RoadBikeRideTest.java
+++ tests/bicycles/RoadBikeRideTest.java
@@ -3,7 +3,7 @@ package bicycles;
 
 import models.RoadBike;
 import org.junit.jupiter.api.Test;
-import rides.BikeRideOne;
+import rides.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,4 +16,14 @@ public class RoadBikeRideTest {
         System.out.println(bikeRide.currentSpeed());
         assertEquals(58, bikeRide.currentSpeed());
     }
+
+    @Test
+    public void roadBikeRideTwoTest() {
+        RoadBike bicycle = new RoadBike();
+        BikeRide bikeRide = new BikeRideTwo(bicycle);
+        bikeRide.ride();
+        System.out.println(bikeRide.currentSpeed());
+        assertEquals(21, bikeRide.currentSpeed());
+    }
+
 }
